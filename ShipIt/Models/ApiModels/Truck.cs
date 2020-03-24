c27c4a63fc9011b55fa78c9156939c84bb19b063
Jamie Baird <james.baird@softwire.com>
2020-03-24 11:32:41 +0000

    Added new class Truck

---
+++ ShipIt/Models/ApiModels/Truck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ShipIt.Models.ApiModels
+{
+    public class Truck
+    {
+        public int Weight { get; set; }
+        public List<Product> ProductList { get; set; }
+
+
+        public Truck()
+        {
+            Weight = 0;
+        }
+
+
+        public int NumberOfItemsOnTruck()
+        {
+            return ProductList.Count;
+        }
+    }
+}
