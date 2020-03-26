f6d29d3c6c7142fe234bc8c090c3d8e62a74f319
Jamie Baird <james.baird@softwire.com>
2020-03-26 15:09:21 +0000

    Deleted duplicated companies as a result of doing db dump twice

---
+++ ShipIt/Migrations/DeleteDuplicateCompanies.sql
@@ -0,0 +1,15 @@
+﻿-- step 1
+CREATE TABLE gcp_temp (LIKE gcp);
+
+-- step 2
+INSERT INTO gcp_temp(gcp_cd, gln_nm, gln_addr_02, gln_addr_03, gln_addr_04, gln_addr_postalcode, gln_addr_city, contact_tel, contact_mail)
+SELECT
+    DISTINCT ON (gcp_cd) gcp_cd, gln_nm, gln_addr_02, gln_addr_03, gln_addr_04, gln_addr_postalcode, gln_addr_city, contact_tel, contact_mail
+FROM gcp;
+
+-- step 3
+DROP TABLE gcp;
+
+-- step 4
+ALTER TABLE gcp_temp
+    RENAME TO gcp; 
